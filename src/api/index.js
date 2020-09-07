1bacc3e869176fcc397d614cf6d03837d314b9e4
Sindre S. Kjær <sindre@sindrekjr.no>
2020-09-07 21:56:03 +0200

    feat(api): add translation support

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,25 @@
 const svada = require('../svada');
+const dotenv = require('dotenv');
 const express = require('express');
+const translate = require('translate');
 
-const { PORT = 3000 } = process.env;
+dotenv.config();
+const { PORT = 3000, TRANS_ENGINE, TRANS_KEY } = process.env;
+
+translate.engine = TRANS_ENGINE;
+translate.key = TRANS_KEY;
+translate.from = 'no';
+
 const app = express();
+const get = request => request.query ? svada(request.query.type) : svada();
+app.get('/', (req, res) => res.send(get(req)));
+app.get('/:language', async (req, res) => {
+  const result = get(req);
+  try {
+    res.send(await translate(get(req), { to: req.params.language }));
+  } catch(Error) {
+    res.send(result);
+  }
+});
 
-app.get('/', (req, res) => res.send(svada()));
-app.get('/:type', (req, res) => res.send(svada(req.params.type)));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
